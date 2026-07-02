c3139f85272795daa6d4b03cf5a17f7e8164edfb
azizbekemail25-svg <azizbekemail25@gmail.com>
2026-07-02 04:31:33 -0700

    app.js
    Add functionality to create and remove films from the list.

--- app.js
+++ app.js
@@ -29,15 +29,18 @@ elConfirmBtn.addEventListener("click", function () {
         alert("Iltimos, barcha maydonlarni to'ldiring");
         return;
     }
+
     const newFilm = {
         name: elTitleInput.value,
         rejisor: elDirectorInput.value,
         reyting: elRatingInput.value,
     };
+
     films.push(newFilm);
 
     const li = document.createElement("li");
     li.textContent = `${newFilm.name} - ${newFilm.rejisor} (${newFilm.reyting})`;
+
     li.addEventListener("click", function () {
         if (confirm(`${li.textContent} — o'chirilsinmi?`)) {
             li.remove();
@@ -45,11 +48,8 @@ elConfirmBtn.addEventListener("click", function () {
             if (idx !== -1) films.splice(idx, 1);
         }
     });
+
     elMovieList.appendChild(li);
 
     elTitleInput.value = "";
-    elDirectorInput.value = "";
-    elRatingInput.value = "";
-    elBackdrop.classList.add("hidden");
-    elModal.classList.add("hidden");
-});
+   
